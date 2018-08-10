72c9ab367f3738a61ae53e015d5212c3dd96902a
Syed Saad <saadhasan620@gmail.com>
2018-08-10 15:20:27 +0000

    add mlab database

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes = require("./routes/comment"),
     
 
 // mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
-mongoose.connect('mongodb://admin:bUS#!1It@ds247121.mlab.com:47121/yelpcamp');
+mongoose.connect('mongodb://admin:hello1@ds247121.mlab.com:47121/yelpcamp');
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
