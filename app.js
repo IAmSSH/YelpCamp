e8558071e109f8fa5de17eecd80c922d4ce94d35
IAmSSH <saadhasan620@gmail.com>
2018-08-19 15:46:35 +0530

    customisation for local use

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-var express         = require("express"), 
-    app             = express(),
-    bodyParser      = require('body-parser'),
-    mongoose        = require("mongoose"),
-    methodOverride  = require("method-override"),
-    Campground      = require("./models/campground"),
+var Campground      = require("./models/campground"),
     Comment         = require("./models/comment"),
-    flash           = require("connect-flash"),
-    passport        = require("passport"),
+    methodOverride  = require("method-override"),
     LocalStrategy   = require("passport-local"),
     User            = require("./models/user"),
-    seedDB          = require("./seeds");
+    flash           = require("connect-flash"),
+    bodyParser      = require('body-parser'),
+    passport        = require("passport"),
+    mongoose        = require("mongoose"),
+    express         = require("express"), 
+    seedDB          = require("./seeds"),
+    app             = express();
     
 var commentRoutes = require("./routes/comment"),
     indexRoutes = require("./routes/index"),
     campgroundRoutes = require("./routes/campground");
     
-
-// mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
-mongoose.connect('mongodb://admin:hello1@ds247121.mlab.com:47121/yelpcamp', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+//mongoose.connect('mongodb://admin:hello1@ds247121.mlab.com:47121/yelpcamp', { useNewUrlParser: true });
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 // seedDB();
-
-
 
 app.use(require("express-session")({
     secret: "Ding Dong",
@@ -48,11 +45,10 @@ app.use(function(req, res, next) {
     next();
 });
 
-
 app.use(indexRoutes);
 app.use(campgroundRoutes);
 app.use(commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log('Server Started!!!');
+app.listen(8080, function() {
+    console.log('Server Started!!');
 });
