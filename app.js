622838b190ca48a59ec647b2cd55ea8114844cbb
Syed Saad <saadhasan620@gmail.com>
2018-08-29 18:45:08 +0000

    change listening port

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use(indexRoutes);
 app.use(campgroundRoutes);
 app.use(commentRoutes);
 
-app.listen(8080, function() {
-    console.log('Server Started!!');
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server Started!!!');
 });
