bb65ac66c3eeb0f13ea879ea3fb5487b3a3f0d4f
Syed Saad <saadhasan620@gmail.com>
2018-11-03 13:16:51 +0000

    add environment var dburl

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes = require("./routes/comment"),
     campgroundRoutes = require("./routes/campground");
     
 // var url = process.env.DATABASEURL;
-// mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
-mongoose.connect('mongodb://admin:hello1@ds247121.mlab.com:47121/yelpcamp', { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect('mongodb://admin:hello1@ds247121.mlab.com:47121/yelpcamp', { useNewUrlParser: true });
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
